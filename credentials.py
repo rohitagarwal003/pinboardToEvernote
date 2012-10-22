f0076c5e0752a795c7015980ebd95afb3e255d3c
Rohit Agarwal <mindprince@gmail.com>
2012-10-22 23:14:12 +0530

    Add instructions to credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,17 @@
-PinboardAPIToken =
-DiffbotToken = 
-EvernoteUsername = 
-EvernotePassword = 
-consumerKey = 
-consumerSecret = 
+# You can find your Pinboard API token on your https://pinboard.in/settings/password page.
+# Finally, the next line should look like:
+# PinboardAPIToken = "username:HEXADECIMALSTRING"
+PinboardAPIToken = ""
+
+# You can get a free Diffbot Developer Token from http://www.diffbot.com/plans/free.
+# Finally, the next line should look like:
+# DiffbotToken = "hexadecimalstring"
+DiffbotToken = ""
+
+# You can get a developer token that allows you to access your own Evernote account.
+# To get a developer token, visit 
+# https://sandbox.evernote.com/api/DeveloperToken.action
+# OR
+# https://www.evernote.com/api/DeveloperToken.action
+EvernoteDeveloperToken = ""
+# Evernote Developer Tokens expire after one year from the date of their creation. So set a reminder to update the developer token after an year.
