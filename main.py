35a08c95bb912bd9146ec8b9100253d4d88b7655
Rohit Agarwal <mindprince@gmail.com>
2012-10-23 05:46:54 +0530

    Add getBookmarksFromDate() method.

--- main.py
+++ main.py
@@ -1,61 +1,82 @@
 #!/usr/bin/python
 
-import traceback
+import traceback, datetime
 from credentials import *
 from pinboardHelper import *
 from diffbotHelper import *
 from sanitize import *
 from evernoteHelper import *
 
-try:
-	recentBookmarkList = getRecentBookmarks(PinboardAPIToken)
-except:
-	print "Fetching bookmarks from Pinboard failed."
-	traceback.print_exc()
-	print
-	exit(1)
-
-notesCreated = 0
-for bookmark in recentBookmarkList:
-
+def main():
 	try:
-		html = extractArticle(DiffbotToken, bookmark[0], html=True)
+		if len(sys.argv) == 1:
+			bookmarkList = getTodaysBookmarks(PinboardAPIToken)
+		elif len(sys.argv) == 2:
+			if sys.argv[1] == "recent":
+				bookmarkList = getRecentBookmarks(PinboardAPIToken)
+			elif sys.argv[1] == "all":
+				bookmarkList = getAllBookmarks(PinboardAPIToken)
+			elif sys.argv[1] == "today":
+				bookmarkList = getTodaysBookmarks(PinboardAPIToken)
+			else:
+				print "Invalid argument"
+				exit(1)
+		elif len(sys.argv) == 4:
+			fromdt = datetime.date(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+			bookmarkList = getBookmarksFromDate(PinboardAPIToken, fromdt)
+		else:
+			print "Wrong Number of arguments"
+			exit(1)
 	except:
-		print "Extracting article using Diffbot failed."
-		print bookmark[0]
+		print "Fetching bookmarks from Pinboard failed."
 		traceback.print_exc()
 		print
-		continue
-		#exit(1)
+		exit(1)
 
-	try:
-		enml = sanitize(html)
-	except:
-		print "Converting article from HTML to ENML failed."
-		print bookmark[0]
-		traceback.print_exc()
-		print
-		continue
-		#exit(1)
+	notesCreated = 0
+	for bookmark in bookmarkList:
 
-	#print
-	#print enml
-	#print
-	#print "length of enml is " + str(len(enml))
-	#print "type of enml is " + str(type(enml))
+		try:
+			html = extractArticle(DiffbotToken, bookmark[0], html=True)
+		except:
+			print "Extracting article using Diffbot failed."
+			print bookmark[0]
+			traceback.print_exc()
+			print
+			continue
+			#exit(1)
 
-	try:
-		sendToEvernote(bookmark[1], bookmark[0], enml, EvernoteDeveloperToken)
-	except:
-		print "Storing note in Evernote failed."
-		print bookmark[0]
-		print enml
-		print
-		traceback.print_exc()
-		print
-		continue
-		#exit(1)
+		try:
+			enml = sanitize(html)
+		except:
+			print "Converting article from HTML to ENML failed."
+			print bookmark[0]
+			traceback.print_exc()
+			print
+			continue
+			#exit(1)
 
-	notesCreated += 1
+		#print
+		#print enml
+		#print
+		#print "length of enml is " + str(len(enml))
+		#print "type of enml is " + str(type(enml))
 
-print "Total number of notes created = " + str(notesCreated)
+		try:
+			sendToEvernote(bookmark[1], bookmark[0], enml, EvernoteDeveloperToken)
+		except:
+			print "Storing note in Evernote failed."
+			print bookmark[0]
+			print enml
+			print
+			traceback.print_exc()
+			print
+			continue
+			#exit(1)
+
+		notesCreated += 1
+
+	print "Total number of notes created = " + str(notesCreated)
+
+if __name__ == "__main__":
+	main()
--- pinboardHelper.py
+++ pinboardHelper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import urllib2 
+import urllib2, datetime 
 from xml.dom.minidom import *
 
 def getRecentBookmarks(auth_token):
@@ -41,7 +41,24 @@ def getTodaysBookmarks(auth_token):
 	Takes the Pinboard API Token (which is of the form username:HEXADECIMALSTRING)
 	Returns a list of (href, title, extended) tuples for all bookmarks created today.
 	"""
-	url = "https://api.pinboard.in/v1/posts/get?auth_token=%s&dt=%s" % (auth_token, str(datetime.date.today()))
+	url = "https://api.pinboard.in/v1/posts/get?auth_token=%s&dt=%s" % (auth_token, str(datetime.datetime.utcnow().date()))
+	rawXML = urllib2.urlopen(url)
+	parsedXML = xml.dom.minidom.parse(rawXML)
+	bookmarkList = []
+	for post in parsedXML.getElementsByTagName("post"):
+		href = post.getAttribute("href").strip()
+		title = post.getAttribute("description").strip()
+		extended = post.getAttribute("extended").strip()
+		bookmark = (href, title, extended)
+		bookmarkList.append(bookmark)
+	return bookmarkList
+
+def getBookmarksFromDate(auth_token, fromdt):
+	"""
+	Takes the Pinboard API Token (which is of the form username:HEXADECIMALSTRING) and a datetime.date() object.
+	Returns a list of (href, title, extended) tuples for all bookmarks created after fromdt.
+	"""
+	url = "https://api.pinboard.in/v1/posts/all?auth_token=%s&fromdt=%s" % (auth_token, str(fromdt))
 	rawXML = urllib2.urlopen(url)
 	parsedXML = xml.dom.minidom.parse(rawXML)
 	bookmarkList = []
